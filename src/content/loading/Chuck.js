b7ab5b0f272c8e21e5844db357150151287e4204
Ben Wong <bwong365@outlook.com>
2018-10-13 16:55:03 -0600

    Chuck spins!

---
+++ src/content/loading/Chuck.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './spaz.css';
+
+const Chuck = () => {
+  return (
+    <div class="spin">
+      <img class="pulse" src="chuck.svg" alt="Chuck Norris" />
+    </div>
+  )
+}
+
+export default Chuck;
